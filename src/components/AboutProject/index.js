71db56b2adcf14ba647432dece9fb914a7b9fe49
Gabriela Alejandra <gabrielasantos70707@gmail.com>
2025-07-30 22:40:55 -0300

    Alterando a descrição do projeto e foto de perfil

--- src/components/AboutProject/index.js
+++ src/components/AboutProject/index.js
@@ -8,12 +8,11 @@ export default function AboutProject() {
       <p className={styles.projectDescription}>
 
         O eLOGiar surgiu com o propósito de transformar em realidade uma visão que nasceu dentro da área de logistica da Bosch.
-        A logística é muito grande, com aproximadamente 300 funcionários que são divididos em 7 áreas diferentes.
+        A logística dentro da Bosch é muito grande, com aproximadamente 300 funcionários que são divididos em 7 áreas diferentes.
 
         Cada gestor é responsável por liderar diversas pessoas, o que torna desafiador acompanhar de perto as atitudes individuais de cada membro da equipe.
-
-
-        Muitas boas atitudes passavam despercebidas e não recebiam o devido reconhecimento. E é por isso que criamos o eLOGiar. 
+        No entanto se cada colaborador pudesse reportar uma boa atitude de um colega, se tornaria possivel a ideia de dar o devido reconhecimento
+        aqueles colaboradores que demonstram boas atitudes. E assim nasceu o eLOGiar.
 
         <span className={styles.projectBordao}>"Boas atitudes não devem ficar invisíveis."</span>
 
